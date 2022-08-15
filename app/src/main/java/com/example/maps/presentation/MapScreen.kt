5e8d6c15a6a5e5563cc1eaaec97a0608da9496fc
Oliver <grahamol@oregonstate.edu>
2022-08-15 16:00:02 -0700

    get dependencies; start the project

---
+++ app/src/main/java/com/example/maps/presentation/MapScreen.kt
@@ -0,0 +1,8 @@
+package com.example.maps.presentation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MapScreen() {
+
+}
---
+++ app/src/main/java/com/example/maps/presentation/MapState.kt
@@ -0,0 +1,7 @@
+package com.example.maps.presentation
+
+import com.google.maps.android.compose.MapProperties
+
+data class MapState(
+    val properties: MapProperties = MapProperties()
+)
